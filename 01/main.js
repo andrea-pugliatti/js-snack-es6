a4108dfc9e81b33e91a5bedf4791401ad5bf8943
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-10-17 13:35:01 +0200

    Make the array of objects in Snack 1

--- 01/main.js
+++ 01/main.js
@@ -3,3 +3,38 @@
  * Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
  * Stampare a schermo la bici con peso minore.
  */
+
+const bici = [
+	{
+		nome: "Genoveffa",
+		peso: 30,
+	},
+	{
+		nome: "Bianchi",
+		peso: 17,
+	},
+	{
+		nome: "Basso",
+		peso: 19,
+	},
+	{
+		nome: "Cinelli",
+		peso: 22,
+	},
+	{
+		nome: "Lombardo",
+		peso: 22,
+	},
+	{
+		nome: "Olmo",
+		peso: 19,
+	},
+	{
+		nome: "Cannondale",
+		peso: 18,
+	},
+	{
+		nome: "Specialized",
+		peso: 18,
+	},
+];
